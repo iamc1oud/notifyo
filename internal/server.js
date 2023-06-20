9c24e6c270e4be68e8972f04c280af00e4f606e2
Ajay Kumar Singh <ajaykumar.kumar900@gmail.com>
2023-06-20 12:15:00 +0530

    Added: Custom argument checks

--- internal/server.js
+++ internal/server.js
@@ -1,11 +1,7 @@
-const express = require('express');
+const expressApp = require('./app.js');
 const mongoose = require('mongoose');
 
 const serve = async () => {
-    const app = express();
-
-    const PORT = process.env.PORT || 9000;
-
     // Connect to database.
     mongoose.connect('mongodb://notifyo_db:27017', {
         authMechanism: 'DEFAULT',
@@ -14,9 +10,11 @@ const serve = async () => {
         console.log(`🛰️ Connected to database`)
     }).catch((e) => {
         console.log("Error connecting to database", e)
-    })
+    });
 
-    app.listen(PORT, () => {
+    const PORT = process.env.PORT || 9000;
+
+    expressApp.listen(PORT, () => {
         console.log(`🚀 Server is listening on: ${PORT}`);
     });
 }
